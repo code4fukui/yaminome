b4a46967e4c958e701f86f3dae33fd1e64a9df47
Taisuke Fukuno <fukuno@jig.jp>
2025-09-10 10:34:05 +0900

    Initial Commit

---
+++ yaminome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yaminome
+//
+//  Created by 福野　泰介 on 2025/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yaminome/yaminomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  yaminomeApp.swift
+//  yaminome
+//
+//  Created by 福野　泰介 on 2025/09/10.
+//
+
+import SwiftUI
+
+@main
+struct yaminomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
